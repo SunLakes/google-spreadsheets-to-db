ff86dbcc306a4408ab3c513b3324e64c62feafac
mibal_ua <9mohapx9@gmail.com>
2023-06-19 21:26:31 +0200

    Implement `SpreadSheetsDataOperator`

---
+++ src/main/java/ua/mibal/googleSpreadsheetsToDb/SpreadSheetsDataOperator.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2023. http://t.me/mibal_ua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.googleSpreadsheetsToDb;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import static ua.mibal.googleSpreadsheetsToDb.utils.GoogleAuthorizeUtil.APPLICATION_NAME;
+import static ua.mibal.googleSpreadsheetsToDb.utils.GoogleAuthorizeUtil.JSON_FACTORY;
+import static ua.mibal.googleSpreadsheetsToDb.utils.GoogleAuthorizeUtil.getCredentials;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link t.me/mibal_ua
+ */
+public class SpreadSheetsDataOperator {
+
+    public List<List<Object>> getData(final String spreadsheetId,
+                                      final String pageName,
+                                      final String range) throws IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+        final String parameter = pageName + "!" + range;
+
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, parameter)
+                .execute();
+        return response.getValues();
+    }
+}
