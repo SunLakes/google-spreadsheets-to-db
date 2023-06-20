0a9941f9954b2fed853e85ad4d83eb4eb894035e
mibal_ua <9mohapx9@gmail.com>
2023-06-20 12:46:05 +0200

    Implement `DatabaseOperator` `writeData()` method
    Use `rowNamesValuesMap` to specify col name and
    lambda with col value. Also implement maps with spreadsheet value and database value in `rowNamesValuesMap`.

--- src/main/java/ua/mibal/googleSpreadsheetsToDb/SpringConfig.java
+++ src/main/java/ua/mibal/googleSpreadsheetsToDb/SpringConfig.java
@@ -1,8 +1,16 @@
 package ua.mibal.googleSpreadsheetsToDb;
 
+import org.springframework.cglib.core.internal.Function;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Mykhailo Balakhon
@@ -12,4 +20,65 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan("ua.mibal.googleSpreadsheetsToDb")
 @PropertySource("classpath:application.properties")
 public class SpringConfig {
+
+    @Bean
+    Map<String, Function<List<Object>, Object>> rowNamesValuesMap() {
+        // 20 червня - 1 день
+        final Map<String, String> DAY_MAP = Map.of(
+                "20", "1",
+                "21", "2",
+                "22", "3",
+                "23", "4",
+                "24", "5",
+                "25", "6",
+                "26", "7",
+                "Всі дні (20-26 липня)", "1,2,3,4,5,6,7"
+        );
+        final Map<String, String> MESSENGER_MAP = Map.of(
+                "Telegram", "Telegram",
+                "Viber", "Viber",
+                "Мессенджер відсутній", "None"
+        );
+        final Map<String, String> TENT_MAP = Map.of(
+                "Так", "T",
+                "Ні", "F"
+        );
+        final Map<String, String> BUS_MAP = Map.of(
+                "На замовному автобусі (приблизна ціна 200 грн в дві сторони)", "T",
+                "Особистим транспортом", "F"
+        );
+
+        return new LinkedHashMap<>() {{
+            put("name", row -> row.get(1).toString().trim());
+            put("primary_phone", row -> row.get(2).toString().trim());
+            put("secondary_phone", row -> row.get(7).toString().trim());
+            put("available_messenger", row -> {
+                final String val = row.get(6).toString();
+                return String.join(",",
+                        Arrays.stream(val.split(", "))
+                                .map(MESSENGER_MAP::get)
+                                .collect(Collectors.toSet())
+                );
+            });
+            put("soc_link", row -> row.get(8).toString().trim());
+            put("city", row -> row.get(10).toString().trim());
+//            TODO put("birth_date", row -> String.join("-",
+//                    row.get(9).toString().split("\\.")));
+            put("days", row -> {
+                final String val = row.get(12).toString();
+                return String.join(",",
+                        Arrays.stream(val.split(", "))
+                                .map(DAY_MAP::get)
+                                .collect(Collectors.toSet()));
+            });
+            put("need_tent", row -> {
+                final String val = row.get(11).toString();
+                return TENT_MAP.get(val);
+            });
+            put("need_bus", row -> {
+                final String val = row.get(13).toString();
+                return BUS_MAP.get(val);
+            });
+        }};
+    }
 }
--- src/main/java/ua/mibal/googleSpreadsheetsToDb/components/DatabaseOperator.java
+++ src/main/java/ua/mibal/googleSpreadsheetsToDb/components/DatabaseOperator.java
@@ -1,12 +1,17 @@
 package ua.mibal.googleSpreadsheetsToDb.components;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.internal.Function;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
+
+import static java.lang.String.format;
 
 /**
  * @author Mykhailo Balakhon
@@ -17,11 +22,14 @@ public class DatabaseOperator {
 
     private final Connection connection;
 
+    private final Map<String, Function<List<Object>, Object>> rowNamesValuesMap;
+
     public DatabaseOperator(@Value("${jdbcDriverPackage}") final String jdbcDriverPackage,
                             @Value("${spring.datasource.url}") final String dbUrl,
                             @Value("${spring.datasource.username}") final String user,
-                            @Value("${spring.datasource.password}") final String password) {
-
+                            @Value("${spring.datasource.password}") final String password,
+                            final Map<String, Function<List<Object>, Object>> rowNamesValuesMap) {
+        this.rowNamesValuesMap = rowNamesValuesMap;
         try {
             Class.forName(jdbcDriverPackage);
             this.connection = DriverManager.getConnection(
@@ -35,6 +43,32 @@ public class DatabaseOperator {
     }
 
     public void writeData(List<List<Object>> data) {
-        // TODO
+        data.forEach(System.out::println);
+        try {
+            insertPerson(data);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void insertPerson(List<List<Object>> data) throws SQLException {
+        final String insertingRowNames = String.join(", ", rowNamesValuesMap.keySet());
+        final String insertingPlaces = "?" + ", ?".repeat(rowNamesValuesMap.size() - 1);
+        final String SQL = format(
+                "INSERT INTO people (%s) VALUES (%S)", insertingRowNames, insertingPlaces
+        );
+        for (List<Object> row : data) {
+            int count = 1;
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            for (Function<List<Object>, Object> func : rowNamesValuesMap.values()) {
+                preparedStatement.setObject(count++, func.apply(row));
+            }
+            System.out.println(preparedStatement);
+            try {
+                preparedStatement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
