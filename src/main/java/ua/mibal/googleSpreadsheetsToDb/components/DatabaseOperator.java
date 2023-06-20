d91d1ab22dc0fc17c5f968d2b18001df4dea0622
mibal_ua <9mohapx9@gmail.com>
2023-06-20 08:53:58 +0200

    Create `DatabaseOperator` component

---
+++ src/main/java/ua/mibal/googleSpreadsheetsToDb/components/DatabaseOperator.java
@@ -0,0 +1,38 @@
+package ua.mibal.googleSpreadsheetsToDb.components;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link t.me/mibal_ua
+ */
+@Component
+public class DatabaseOperator {
+
+    private final Connection connection;
+
+    public DatabaseOperator(@Value("${jdbcDriverPackage}") final String jdbcDriverPackage,
+                            @Value("${dbUrl}") final String dbUrl,
+                            @Value("${user}") final String user,
+                            @Value("${password}") final String password) {
+        try {
+            Class.forName(jdbcDriverPackage);
+            this.connection = DriverManager.getConnection(
+                    dbUrl,
+                    user,
+                    password);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void writeData(List<List<Object>> data) {
+        // TODO
+    }
+}
